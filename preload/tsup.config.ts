9951816aafa22f304b8a3e9ff70d4a02af24fa14
Kiet Ho <hoakiet98@gmail.com>
2025-03-24 14:16:54 -0700

    Add preload script

---
+++ preload/tsup.config.ts
@@ -0,0 +1,12 @@
+// tsup.config.ts
+import { defineConfig } from 'tsup';
+
+export default defineConfig({
+    entry: ['src/index.ts'],
+    format: ['esm'],
+    bundle: true,
+    clean: true,
+    dts: false,
+    minify: true,
+    outDir: 'dist'
+});
